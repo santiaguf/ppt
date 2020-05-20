03535df689bc33edba76815d7d5663824964164c
Santiago Bernal B <exactlimon@gmail.com>
2020-05-19 23:37:47 -0500

    [add] first version completed

--- src/main/java/com/santiaguf/ppt/Main.java
+++ src/main/java/com/santiaguf/ppt/Main.java
@@ -6,6 +6,7 @@
 package com.santiaguf.ppt;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  *
@@ -13,18 +14,44 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args){
-        showMenu();
+        Scanner sc = new Scanner(System.in);
+        showMenu(sc);
     }
     
-    public static void showMenu(){
-        Scanner sc = new Scanner(System.in);
-        int opcion = -1;
+    public static void showMenu(Scanner sc){
+        int option = 0;
         do{
             System.out.println("-----------------\n"
                     + " Piedra, Papel o Tijera \n"
                     + "1. Jugar \n"
                     + "0. Salir");
-            opcion = sc.nextInt();        
-        }while(opcion != 0);
+            option = sc.nextInt();
+            if(option == 1){
+                playGame(sc);
+            }
+        }while(option != 0);
+    }
+    
+    public static void playGame(Scanner sc){
+        System.out.println("-----------------\n"
+                    + "Selecciona una opción y espera que la pc escoja otra"
+                    + " 1. Piedra, 2. Papel o 3.Tijera \n");
+        int option = sc.nextInt();
+        getResult(option);
+    }
+    
+    public static void getResult(int userOption){
+
+        if(userOption == 3){
+            userOption = 0;
+        }
+
+        String validOptions[] = {"Tijera","Piedra","Papel"};
+        String matchs[][] = {{"Empate","Pierdes","Ganas"},{"Ganas","Empate","Pierdes"},{"Pierdes","Ganas","Empate"}};
+        
+        int machineOption = ThreadLocalRandom.current().nextInt(0, 2 + 1);
+        
+        System.out.println("La Pc seleccionó: "+validOptions[machineOption]);
+        System.out.println("!!"+matchs[userOption][machineOption]+"\n");    
     }
 }
